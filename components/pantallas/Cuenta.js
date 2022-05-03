1e6d1a587c9d26720b57d3b29ba4e63af7b70e04
AxelFlores1609 <axelillogg@gmail.com>
2022-05-02 19:04:27 -0500

    CuentaUpdate

--- components/pantallas/Cuenta.js
+++ components/pantallas/Cuenta.js
@@ -1,98 +1,139 @@
 import React from "react";
-import { Text } from "react-native"
+import { Text } from "react-native";
 import {
-    Heading, Input, Center, NativeBaseProvider, NativeProvider, Button, Box, AspectRatio, Image, Stack,
-    HStack, ScrollView, FlatList, View, Pressable, selected, Icon, Link, VStack, Skeleton, Container
-} from 'native-base'
+  Heading,
+  Input,
+  Avatar,
+  Center,
+  NativeBaseProvider,
+  NativeProvider,
+  Button,
+  Box,
+  AspectRatio,
+  Image,
+  Stack,
+  HStack,
+  ScrollView,
+  FlatList,
+  View,
+  Pressable,
+  selected,
+  Icon,
+  Link,
+  VStack,
+  Skeleton,
+  Container,
+} from "native-base";
 
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 const Cuenta = ({ navigation }) => {
+  return (
+    <NativeBaseProvider>
+      <Center bg="primary.400">
+        <Center
+          bg="primary.400"
+          _text={{
+            color: "white",
+            fontWeight: "bold",
+          }}
+          height={200}
+          width={{
+            base: 200,
+            lg: 250,
+          }}
+        >
+          <Avatar
+            bg="green.500"
+            size="xl"
+            source={{
+              uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            }}
+          >
+            AJ
+          </Avatar>
+          <Text style={{ color: '#FFFFFF' }}>Jennifer Flores</Text>
+          <Text style={{ color: '#FFFFFF' }}>Avanzado</Text>
+        </Center>
+      </Center>
 
-   
-    return(
-        <NativeBaseProvider>
-            <Container></Container>            
+      <Box flex={1} bg="white" safeAreaTop>
+        <Center flex={1}></Center>
+        <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
+          <Pressable
+            opacity={selected === 0 ? 1 : 0.5}
+            py="3"
+            flex={1}
+            onPress={() => navigation.navigate("HomeScreen")}
+          >
+            <Center>
+              <Icon
+                mb="1"
+                as={
+                  <MaterialCommunityIcons
+                    name={selected === 0 ? "home" : "home-outline"}
+                  />
+                }
+                color="white"
+                size="sm"
+              />
+              <Text color="white" fontSize="12">
+                Home
+              </Text>
+            </Center>
+          </Pressable>
 
+          <Pressable
+            opacity={selected === 2 ? 1 : 0.6}
+            py="2"
+            flex={1}
+            onPress={() => navigation.navigate("MisServicios")}
+          >
+            <Center>
+              <Icon
+                mb="1"
+                as={
+                  <MaterialCommunityIcons
+                    name={selected === 3 ? "account" : "account-outline"}
+                  />
+                }
+                color="white"
+                size="sm"
+              />
+              <Text color="white" fontSize="12">
+                MisServicios
+              </Text>
+            </Center>
+          </Pressable>
 
-            <Box flex={1} bg="white" safeAreaTop>
-                <Center flex={1}></Center>
-                <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
-                    <Pressable
-                        opacity={selected === 0 ? 1 : 0.5}
-                        py="3"
-                        flex={1}
-                        onPress={() => navigation.navigate('HomeScreen')}>
-                        <Center>
-                            <Icon
-                                mb="1"
-                                as={
-                                    <MaterialCommunityIcons
-                                        name={selected === 0 ? 'home' : 'home-outline'}
-                                    />
-                                }
-                                color="white"
-                                size="sm"
-                            />
-                            <Text color="white" fontSize="12">
-                                Home
-                            </Text>
-                        </Center>
-                    </Pressable>
-
-                    <Pressable
-                        opacity={selected === 2 ? 1 : 0.6}
-                        py="2"
-                        flex={1}
-                        onPress={() => navigation.navigate('MisServicios')}
-                    >
-                        <Center>
-                            <Icon
-                                mb="1"
-                                as={
-                                    <MaterialCommunityIcons
-                                        name={selected === 3 ? 'account' : 'account-outline'}
-                                    />
-                                }
-                                color="white"
-                                size="sm"
-                            />
-                            <Text color="white" fontSize="12">
-                                MisServicios
-                            </Text>
-                        </Center>
-                    </Pressable>
-
-                    <Pressable
-                        opacity={selected === 2 ? 1 : 0.6}
-                        py="2"
-                        flex={1}
-                        onPress={() => navigation.navigate('Cuenta')}
-                    >
-                        <Center>
-                            <Icon
-                                mb="1"
-                                as={
-                                    <MaterialCommunityIcons
-                                        name={selected === 3 ? 'account' : 'account-outline'}
-                                    />
-                                }
-                                color="white"
-                                size="sm"
-                            />
-                            <Text color="white" fontSize="12">
-                                Cuenta
-                            </Text>
-                        </Center>
-                    </Pressable>
-                </HStack>
-            </Box>
-            </NativeBaseProvider>
-    )
-}
-
+          <Pressable
+            opacity={selected === 2 ? 1 : 0.6}
+            py="2"
+            flex={1}
+            onPress={() => navigation.navigate("Cuenta")}
+          >
+            <Center>
+              <Icon
+                mb="1"
+                as={
+                  <MaterialCommunityIcons
+                    name={selected === 3 ? "account" : "account-outline"}
+                  />
+                }
+                color="white"
+                size="sm"
+              />
+              <Text color="white" fontSize="12">
+                Cuenta
+              </Text>
+            </Center>
+          </Pressable>
+        </HStack>
+      </Box>
+    </NativeBaseProvider>
+  );
+};
 
 export default Cuenta;
-
