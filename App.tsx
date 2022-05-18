d31b5f9557b97e8b38d446ac71fef18c1940118f
Lemus1105 <79469920+Lemus1105@users.noreply.github.com>
2022-05-18 13:25:50 -0500

    Revert "que esta pasando"
    This reverts commit 8a7369ac16a63286d30972fdd02084a2d9ed8b43.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import login from "./component/login"
+import busqueda from "./component/busqueda"
 
+export default function App() {
+  return (
+      <login /> 
+      <busqueda />   
 
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
