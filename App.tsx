8a7369ac16a63286d30972fdd02084a2d9ed8b43
Lemus1105 <79469920+Lemus1105@users.noreply.github.com>
2022-05-18 13:21:52 -0500

    que esta pasando

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import login from "./component/login"
-import busqueda from "./component/busqueda"
 
-export default function App() {
-  return (
-      <login /> 
-      <busqueda />   
 
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
