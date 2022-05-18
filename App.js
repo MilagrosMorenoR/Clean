fdf2fd9968b5d09efaa2b1059929093fa18d49bd
Lemus1105 <79469920+Lemus1105@users.noreply.github.com>
2022-05-18 13:28:37 -0500

    hola

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import CleanStack from 'clean/components/pantallas/CleanStack'
+import { StyleSheet, View, Button, TextInput  } from 'react-native';
+import { reduceEachLeadingCommentRange } from 'typescript';
 
-function App () {
+
+export default function App() {
+  const [text, onChangeEstado] = React.useState("Codigo postal");
+ 
+
   return (
-    
-      <CleanStack/>
-    
-  )
-};
+    <View style={styles.container}>
+      <TextInput onChange={onChangeEstado} style={styles.input} value={text} />
+      
+      
+      <Button
+        title="Buscar"
+        onPress={() => console.log("holaaa")}
+      />
+    </View>
+  );
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 60,
+    borderWidth: 3,
+    borderRadius: 12,
+    
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 12,
+  },
+  Button:{
+    borderRadius: 12,
+    width: 12,
+  },
+});
