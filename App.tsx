d20a9bd06199a200b7089b3418e873a94779b094
AxelFlores1609 <axelillogg@gmail.com>
2022-05-18 13:53:43 -0500

    NO MODIFIQUEN EL PACKAGE.JSON

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import login from "./component/login"
-import busqueda from "./component/busqueda"
 
 export default function App() {
   return (
-      <login /> 
-      <busqueda />   
-
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
