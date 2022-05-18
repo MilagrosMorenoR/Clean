3c4f90c08747ac1569b2aa81cf2f13c231e6178d
Lemus1105 <79469920+Lemus1105@users.noreply.github.com>
2022-05-18 13:24:27 -0500

    xd

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+<<<<<<< HEAD
 
+=======
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+>>>>>>> parent of 258a953 (buscar)
 
 const styles = StyleSheet.create({
   container: {
