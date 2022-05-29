29fb039995744c57943b6f763a0c5c2c4520775d
AxelFlores1609 <axelillogg@gmail.com>
2022-05-29 00:16:42 -0500

    Ya se como usar la bd

--- components/pantallas/Paquetes.js
+++ components/pantallas/Paquetes.js
@@ -28,17 +28,40 @@ import {
   Container,
 } from "native-base";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import firebase from '../pantallas/firebase-config';
 
-
-const Paquetes = ({ navigation }) => {
+const Paquetes = ({ navigation, data }) => {
 
   
+const [paquetes, setPaquetes] = useState([])
+useEffect(() => {
+  firebase
+  .db
+  .collection("Paquete")
+  .onSnapshot((querySnapshot) => {
+    const paquetes = [];
+    querySnapshot.docs.forEach((doc) => {
+      const { Descripcion, Monto, Nombre, Imagen } = doc.data();
+      const Id = doc.id;
+      paquetes.push({
+        id: Id,
+        Descripcion: Descripcion, 
+        Monto: Monto,
+        Imagen:  Imagen, 
+        Nombre: Nombre,
+      });
+    });
+    setPaquetes(paquetes);
+  });
+}, []);
 
   return (
     <NativeBaseProvider>
       <ScrollView>
+      {paquetes.map((paquetes) => {
+          return (
       <Center backgroundColor={"#FFFFFF"}>
-        <Box alignItems="center">
+        <Box alignItems="center" marginTop={20}>
           <Box
             maxW="80"
             rounded="lg"
@@ -58,21 +81,23 @@ const Paquetes = ({ navigation }) => {
             }}
             pt={5}
           >
+            
             <Box>
               <AspectRatio w="100%" ratio={16 / 9}>
                 <Image
                   source={{
-                    uri: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2018/06/limpieza-casa-cubo-fregona.jpg",
+                    uri: paquetes.Imagen,
                   }}
                   alt="image"
                 />
               </AspectRatio>
               
             </Box>
+            
             <Stack p="4" space={3}>
               <Stack space={2}>
                 <Heading size="md" ml="-1">
-                  Limpiar casa
+                  {paquetes.Nombre}
                 </Heading>
                 <Heading size="md" ml="-1">
                   8:00 am
@@ -93,7 +118,7 @@ const Paquetes = ({ navigation }) => {
                 </Text>
               </Stack>
               <Text fontWeight="400">
-              Realizar limpieza de 2 baños, cochera, patio y dos habitaciones.
+              {paquetes.Descripcion}
               </Text>
               <HStack
                 alignItems="center"
@@ -108,98 +133,21 @@ const Paquetes = ({ navigation }) => {
                     }}
                     fontWeight="400"
                   >
-                   Pago de: $600
+                   {paquetes.Monto} $
                   </Text>
                 </HStack>
 
                 <Button onPress={() => navigation.navigate("MisServicios")}>Seleccionar</Button>
               </HStack>
             </Stack>
+          
           </Box>
+          
         </Box>
+        
       </Center>
-
-      <Center backgroundColor={"#FFFFFF"}>
-        <Box alignItems="center">
-          <Box
-            maxW="80"
-            rounded="lg"
-            overflow="hidden"
-            borderColor="coolGray.200"
-            borderWidth="1"
-            _dark={{
-              borderColor: "coolGray.600",
-              backgroundColor: "gray.700",
-            }}
-            _web={{
-              shadow: 2,
-              borderWidth: 0,
-            }}
-            _light={{
-              backgroundColor: "gray.50",
-            }}
-            pt={5}
-          >
-            <Box>
-              <AspectRatio w="100%" ratio={16 / 9}>
-                <Image
-                  source={{
-                    uri: "https://cdn.autobild.es/sites/navi.axelspringer.es/public/media/image/2018/06/limpieza-casa-cubo-fregona.jpg",
-                  }}
-                  alt="image"
-                />
-              </AspectRatio>
-              
-            </Box>
-            <Stack p="4" space={3}>
-              <Stack space={2}>
-                <Heading size="md" ml="-1">
-                  Limpiar casa
-                </Heading>
-                <Heading size="md" ml="-1">
-                  13:00 pm
-                </Heading>
-                <Text
-                  fontSize="xs"
-                  _light={{
-                    color: "violet.500",
-                  }}
-                  _dark={{
-                    color: "violet.400",
-                  }}
-                  fontWeight="500"
-                  ml="-0.5"
-                  mt="-1"
-                >
-                  Detalles:
-                </Text>
-              </Stack>
-              <Text fontWeight="400">
-              Realizar limpieza de 2 baños, cochera, patio y dos habitaciones.
-              </Text>
-              <HStack
-                alignItems="center"
-                space={4}
-                justifyContent="space-between"
-              >
-                <HStack alignItems="center">
-                  <Text
-                    color="coolGray.600"
-                    _dark={{
-                      color: "warmGray.200",
-                    }}
-                    fontWeight="400"
-                  >
-                    Pado de: $600
-                  </Text>
-                </HStack>
-
-                <Button onPress={() => navigation.navigate("MisServicios")}>Seleccionar</Button>
-              </HStack>
-            </Stack>
-          </Box>
-        </Box>
-      </Center>
+          );
+                  })}
 
 
       <Box flex={1} bg="white" safeAreaTop>
